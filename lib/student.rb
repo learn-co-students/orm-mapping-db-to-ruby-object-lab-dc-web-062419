8cef4e1ab118404bd611d0e32fb4149ea74866c3
github username <github email address>
2019-07-03 11:42:54 -0400

    Just committing more often; half of the deliverables are there but column-to-variable mapping could be more flexi

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,36 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    # There has to be a more flexible way to match variable names to columns. Did we do that last time with a k,v pair?
+    noob= self.new
+    noob.id = row[0]
+    noob.name = row[1]
+    noob.grade = row[2]
+    noob
   end
+
+  @@all = []
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+    # what_we_get = DB[:conn].execute(sql)
+    DB[:conn].execute(sql).each {|row| @@all << self.new_from_db(row)}
+    # what_we_get.each {|row| @@all << self.new_from_db(row)}
+    @@all
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    binding.pry
   end
   
   def save
